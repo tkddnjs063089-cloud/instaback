d086aff2a28d075734b50df5949563d30ace1da6
sang won <tkddnjs063089@gmail.com>
2025-12-26 12:51:46 +0900

    Fix refreshToken type

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -22,8 +22,8 @@ export class User {
   @Column({ name: 'profile_image', nullable: true })
   profileImage: string;
 
-  @Column({ name: 'refresh_token', nullable: true })
-  refreshToken: string | null; // 해시된 refresh token 저장
+  @Column({ name: 'refresh_token', nullable: true, type: 'text' })
+  refreshToken: string; // 해시된 refresh token 저장
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
